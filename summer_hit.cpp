a819ffb042925e4bbb94ecde6cfc7ddb9e9e6800
gandhi-sanjana <72259129+gandhi-sanjana@users.noreply.github.com>
2021-10-20 22:35:39 +0530

    Create summer_hit.cpp

---
+++ summer_hit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    long long int t,i;
+    cin>>t;
+for (i=0;i<t;i++)
+{
+long long int a,b,c,d, result;
+cin>>a>>b>>c>>d;
+result=c/a+d/b;
+cout<<result<<"\n";
+}
+return 0;
+}
